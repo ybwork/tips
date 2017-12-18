810fadd18a93853609032bdb0f7ed41b83da586d
Ilya Kaduk <kaduk9393@gmail.com>
2017-12-18 17:49:29 +0300

    V-5.

---
+++ laravel/laravel.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+	Внедрение injection dependency.
+
+	Связь реализации с интрефейсов делается через service provider.
+
+	Пример:
+
+		public function register()
+	    {
+	        $this->app->bind(
+	            'App\Services\File\IFile',
+	            'App\Services\File\YBFile'
+	        );
+	    }
+	
+	Код выше аналогичен моему кода на чистом php:
+
+		class User
+		{
+			private $user;
+
+			public function __construct(IUserModel $user)
+			{
+				$this->user = $user;	
+			}
+		}
+
+		$file = new User();
+		$file->set_model(new MySQLUserModel);
+*/
+
