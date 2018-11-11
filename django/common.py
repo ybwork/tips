e57aca11898d4427f31f487057e05946756636a1
yetbetter <costilek@gmail.com>
2018-11-11 15:16:12 +0300

    V-107

--- django/common.py
+++ django/common.py
@@ -1,7 +1,7 @@
 '''
 	Оглавление:
 
-		Модели
+		Модели:
 
 		Один ко многим
 
@@ -28,6 +28,36 @@
 		Множественное наследование
 
 		Переопределение полей при наследовании
+
+		Выполнение запросов:
+
+		Создание
+
+		Обновление
+
+		Получение
+
+		Ограничение выборки
+
+		Фильтры
+
+		Фильтры по связанным объектам
+
+		Сравнение одного поля с другим
+
+		Кэширование и QuerySets
+
+		Сложные запросы с помощью объектов Q
+
+		Сравнение объектов
+
+		Удаление
+
+		Копирование объекта
+
+		Агрегация (min/max, count, avg)
+
+		Транзакции
 '''
 
 # Модели
@@ -240,7 +270,7 @@ super(Blog, self).save(*args, **kwargs)
 
 	Если дочерняя модель хочет расширить родительский Meta класс, она может унаследовать его. 
 
-	Используя атрибут related_name для ForeignKey или ManyToManyField, вы должны всегда определять уникальное название для обратной связи.
+	Используя атрибут related_name для ForeignKey или ManyToManyField, вы должны всегда определять уникальное название для обратной связи. Это имя будет использовано вместо field_set в выражении b.field_set.all()
 '''
 class CommonInfo(models.Model):
     name = models.CharField(max_length=100)
@@ -314,3 +344,440 @@ class MyPerson(Person):
 '''
 	В Python можно переопределять атрибуты класса-родителя в дочернем классе. В Django это запрещено для атрибутов, которые являются экземплярами Field. Если родительская модель имеет поле author, вы не можете создать поле с именем author в дочерних моделях.
 '''
+
+# Выполнение запросов
+'''
+	Класс модели представляет таблицу, а экземпляр модели - запись в этой таблице.
+
+	Чтобы создать объект, нужно создать экземпляр класса модели.
+'''
+class Blog(models.Model):
+    name = models.CharField(max_length=100)
+    tagline = models.TextField()
+
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+
+class Entry(models.Model):
+    blog = models.ForeignKey(Blog)
+    headline = models.CharField(max_length=255)
+    body_text = models.TextField()
+    pub_date = models.DateField()
+    mod_date = models.DateField()
+    authors = models.ManyToManyField(Author)
+    comments = models.IntegerField()
+    pingbacks = models.IntegerField()
+    rating = models.IntegerField()
+
+# Создание
+'''
+	Чтобы создать объект, создайте экземпляр класса модели, указав необходимые поля в аргументах и вызовите метод save() чтобы сохранить его в базе данных.
+'''
+b = Blog(name='Beatles Blog', tagline='All the latest Beatles news.')
+b.save()
+
+# Обновление
+'''
+	Для сохранения изменений в объект, который уже существует в базе данных, используйте save().
+
+	Обновление ForeignKey работает так же, как и сохранение обычных полей, просто назначьте полю объект необходимого типа.
+
+	Обновление ManyToManyField работает немного по-другому, используйте метод add() поля, чтобы добавить связанный объект.
+
+	Метод update() использует непосредственно SQL запрос. Это операция для массового изменения, при этом метод save() модели не будет вызван и сигналы pre_save и post_save не сработают.
+'''
+b.name = 'New name'
+b.save()
+
+# ForeignKey update
+entry = Entry.objects.get(pk=1)
+cheese_blog = Blog.objects.get(name="Cheddar Talk")
+entry.blog = cheese_blog
+entry.save()
+
+# ManyToManyField update
+joe = Author.objects.create(name="Joe")
+entry.authors.add(joe)
+
+# multiple ManyToManyField update
+john = Author.objects.create(name="John")
+paul = Author.objects.create(name="Paul")
+entry.authors.add(john, paul)
+
+# обновление нескольких объектов, которые соотвествуют условию
+Entry.objects.filter(
+	pub_date__year=2007
+).update(
+	headline='Everything is the same'
+)
+
+# обновляет все записи в связанном объекте
+b = Blog.objects.get(pk=1)
+Entry.objects.all().update(blog=b)
+
+# если нужно сохранить каждый объект в QuerySet и удостовериться что метод save() вызван для каждого объекта
+for item in my_queryset:
+    item.save()
+
+Entry.objects.all().update(pingbacks=F('pingbacks') + 1)
+
+# Получение
+'''
+	Для получения объектов из базы данных, создается QuerySet через Manager модели.
+
+	QuerySet представляет выборку объектов из базы данных.
+
+	Обратиться к менеджерам можно только через модель и нельзя через ее экземпляр.
+
+	После каждого изменения QuerySet, вы получаете новый QuerySet, который никак не связан с предыдущим QuerySet.
+
+	QuerySets – ленивы, создание QuerySet не выполняет запросов к базе данных, пока QuerySet не вычислен.
+'''
+Blog.objects.all()
+
+Entry.objects.filter(pub_date__year=2006)
+
+Entry.objects.filter(
+	headline__startswith='What'
+).exclude(
+	pub_date__gte=datetime.date.today()
+)
+
+# уникальность QuerySet
+q1 = Entry.objects.filter(headline__startswith="What")
+q2 = q1.exclude(pub_date__gte=datetime.date.today())
+q3 = q1.filter(pub_date__gte=datetime.date.today())
+
+# ленивость QuerySet
+q = Entry.objects.filter(headline__startswith="What")
+q = q.exclude(pub_date__gte=datetime.date.today())
+q = q.filter(pub_date__gte=datetime.date.today())
+print(q) # запрос был выполнен только сейчас и только один
+
+# если объекта нет, то выбросит исключение DoesNotExist или MultipleObjectsReturned если запрос вернул больше одной записи
+one_entry = Entry.objects.get(pk=1)
+
+# получение объектов по связи один ко многим
+e = Entry.objects.get(id=2)
+e.blog
+
+b = Blog.objects.get(id=1)
+b.entry_set.all()
+
+# получение объектов по связи многие ко многим
+e = Entry.objects.get(id=3)
+e.authors.all()
+e.authors.count()
+e.authors.filter(name__contains='John')
+
+a = Author.objects.get(id=5)
+a.entry_set.all()
+
+# получение объектов по связи один к одному
+class EntryDetail(models.Model):
+    entry = models.OneToOneField(Entry, on_delete=models.CASCADE)
+    details = models.TextField()
+
+ed = EntryDetail.objects.get(id=2)
+ed.entry
+
+e = Entry.objects.get(id=2)
+e.entrydetail
+
+# запросы со связанными объектами (все 3 запроса идентичны)
+Entry.objects.filter(blog=b)
+Entry.objects.filter(blog=b.id)
+Entry.objects.filter(blog=5)
+
+# Ограничение выборки
+'''
+	Для ограничения результата выборки QuerySet используются срезы. Они эквивалент таких операторов SQL как LIMIT и OFFSET.
+
+	QuerySet возвращает новый QuerySet и запрос не выполняется. Исключением является использовании “шага” в срезе.
+
+	Отрицательные срезы (например, Entry.objects.all()[-1]) не поддерживаются.
+
+	Именованные аргументы функции filter() и др. – объединяются оператором 'AND'.
+'''
+# возвращает первые 5 объектов
+Entry.objects.all()[:5] 
+
+# для получения одного объекта используйте индекс вместо среза
+Entry.objects.order_by('headline')[0] 
+
+# Точное совпадение
+Entry.objects.get(id__exact=14)
+
+
+# Фильтры
+
+	# Точное совпадение, регистро-независимое
+	Entry.objects.filter(name__iexact='beatles blog')
+
+	# Регистрозависимая проверка на вхождение
+	Entry.objects.filter(headline__contains='Lennon')
+
+	# знак % экранировать не нужно
+	Entry.objects.filter(headline__contains='%')
+
+	# Регистронезависимая проверка на вхождение
+	Entry.objects.filter(headline__icontains='Lennon')
+
+	# Больше чем
+	Entry.objects.filter(id__gt=4)
+
+	# Больше чем или равно
+	Entry.objects.filter(id__gte=4)
+
+	# Меньше чем
+	Entry.objects.filter(id__lt=4)
+
+	# Меньше чем или равно
+	Entry.objects.filter(id__lte=4)
+
+	# Регистрозависимая проверка начинается ли поле с указанного значения
+	Entry.objects.filter(id__startswith=4)
+
+	# Регистронезависимая проверка начинается ли поле с указанного значения
+	Entry.objects.filter(id__istartswith=4)
+
+	# Регистрозависимая проверка оканчивается ли поле с указанного значения
+	Entry.objects.filter(id__endswith=4)
+
+	# Регистронезависимая проверка оканчивается ли поле с указанного значения
+	Entry.objects.filter(id__iendswith=4)
+
+	# Проверка на вхождение в диапазон (включающий)
+	Entry.objects.filter(pub_date__range=(start_date, end_date))
+
+	# Проверка на дату
+	Entry.objects.filter(pub_date__date=datetime.date(2005, 1, 1))
+
+	# Проверяет на IS NULL
+	Entry.objects.filter(pub_date__isnull=True)
+
+	# Проверяет на IS NOT NULL
+	Entry.objects.filter(pub_date__isnull=False) 
+
+	# Полнотекстовый поиск, который использует преимущества полнотекстового индекса. Работает как и contains но значительно быстрее благодаря полнотекстовому индексу
+	Entry.objects.filter(headline__search="+Django -jazz Python")
+
+	# Регистрозависимая проверка регулярным выражением
+	Entry.objects.filter(title__regex=r'^(An?|The) +')
+
+	# Регистронезависимая проверка регулярным выражением
+	Entry.objects.filter(title__iregex=r'^(an?|the) +')
+
+# Фильтры по связанным объектам
+'''
+	Для фильтра по полю из связанных моделей, используйте имена связывающих полей разделенных двойным нижним подчеркиванием, пока вы не достигните нужного поля.
+'''
+
+# получает все объекты Entry с Blog, name которого равен 'Beatles Blog'
+Entry.objects.filter(blog__name='Beatles Blog')
+
+# получает все объекты Blog с Entry, headline которого равен 'Lennon'
+Blog.objects.filter(entry__headline='Lennon')
+
+# фильтр через несколько связей
+Blog.objects.filter(entry__authors__name='Lennon')
+
+# Фильтрация по связям многие-ко-многим. Для выбора всех блогов, содержащих записи и с 'Lennon' в заголовке и опубликованные в 2008 (запись должна удовлетворять оба условия)
+Blog.objects.filter(
+	entry__headline__contains='Lennon',
+    entry__pub_date__year=2008
+)
+
+# Фильтрация по связям многие-ко-многим. Для выбора блогов с записями, у которых заголовок содержит “Lennon”, а также с записями опубликованными в 2008
+Blog.objects.filter(
+	entry__headline__contains='Lennon'
+).filter(
+    entry__pub_date__year=2008
+)
+
+
+# Исключения по связям многие-ко-многим. запрос исключит блоги, с записями, у которых заголовок содержит “Lennon”, а также с записями опубликованными в 2008
+Blog.objects.exclude(
+    entry__headline__contains='Lennon',
+    entry__pub_date__year=2008,
+)
+
+# Сравнение одного поля с другим
+'''
+	Django предоставляет класс F для сравнений одного поля с другим. Экземпляр F() рассматривается как ссылка на другое поле модели. Эти ссылки могут быть использованы для сравнения значений двух разных полей одного объекта модели.
+
+	Django поддерживает операции суммирования, вычитания, умножения, деления и арифметический модуль для объектов F(), с константами или другими объектами F()
+'''
+
+# выбирает все записи, у которых количество комментариев больше, чем pingbacks. pingbacks это поле в этой же таблице
+Entry.objects.filter(comments__gt=F('pingbacks'))
+
+Entry.objects.filter(comments__gt=F('pingbacks') * 2)
+
+# Кэширование и QuerySets
+'''
+	В только что созданном QuerySet кеш пустой. После вычисления QuerySet будет выполнен запрос к базе данных. После этого Django сохраняет результат запроса в кеше QuerySet и возвращает необходимый результат.
+
+	Последующие вычисления QuerySet используют кеш.
+
+	Если не сохранять результат запроса в переменную, исппользуя при этом срез или индекс, кэш не сработает.
+'''
+
+# этот код создаст два экземпляра QuerySet и вычислит их не сохраняя, что увеличит нагрузку (плохой подход)
+print([e.headline for e in Entry.objects.all()])
+print([e.pub_date for e in Entry.objects.all()])
+
+# используется кэш
+queryset = Entry.objects.all()
+print([p.headline for p in queryset])
+print([p.pub_date for p in queryset]) # здесь уже запрос из кэша
+
+queryset = Entry.objects.all()
+print(queryset[5]) # не будет использоваться кэш
+
+queryset = Entry.objects.all()
+[entry for entry in queryset]
+print(queryset[5]) # будет использоваться кэш
+
+# Сложные запросы с помощью объектов Q
+'''
+	Если вам нужны более сложные запросы (например, запросы с оператором OR), вы можете использовать объекты Q.
+
+	Объекты Q могут быть объединены операторами & и |, при этом будет создан новый объект Q.
+
+	Если присутствует объект Q, он должен следовать перед именованными аргументами.
+'''
+Q(question__startswith='Who') | Q(question__startswith='What')
+
+# SELECT * from polls WHERE question LIKE 'Who%' AND (pub_date = '2005-05-02' OR pub_date = '2005-05-06')
+Poll.objects.get(
+    Q(question__startswith='Who'),
+    Q(pub_date=date(2005, 5, 2)) | Q(pub_date=date(2005, 5, 6))
+)
+
+# Сравнение объектов
+'''
+	Для сравнения двух экземпляров модели, используйте стандартный оператор сравнения ==. При этом будут сравнены первичные ключи.
+'''
+some_entry == other_entry
+
+# Удаление
+'''
+	Метод delete() сразу удаляет объект и возвращает количество удаленных объектов, и словарь с количеством удаленных объектов для каждого типа.
+
+	(1, {'weblog.Entry': 1})
+'''
+b = Blog.objects.get(pk=1)
+b.delete()
+
+# удаляет все объекты, которые попадают под условие
+Entry.objects.filter(pub_date__year=2005).delete()
+
+Entry.objects.all().delete()
+
+# Копирование объекта
+'''
+	Для копирования существующего объекта нужно создать новый экземпляр с копией всех полей другого объекта, установить pk в None и сохранить.
+
+	Если используется наследование, то нужно установить pk и id в None.
+'''
+blog = Blog(name='My blog', tagline='Blogging is easy')
+blog.save()
+blog.pk = None
+blog.save()
+
+django_blog = Blog(name='My blog', tagline='Blogging is easy')
+django_blog.save()
+django_blog.pk = None
+django_blog.id = None
+django_blog.save()
+
+# копирование связанных объектов
+entry = Entry.objects.all()[0]
+old_authors = entry.authors.all()
+entry.pk = None
+entry.save()
+entry.authors = old_authors
+
+# Агрегация
+'''
+	Django предоставляет два способа использовать агрегацию. Первый способ заключается в использовании агрегации для всех объектов QuerySet, второй для каждого объекта.
+
+	Для вычисления значения для всех объектов используется aggregate, для вычисления значения для каждого объекта используется annotate.
+'''
+
+# для вычисления среднего значения для всех объектов, aggregate() завершающая инструкция для QuerySet, которая возвращает словарь с результатом
+Book.objects.aggregate(Avg('price')) # {'price__avg': 34.35}
+
+# если нужно вычислить больше одного значения
+Book.objects.aggregate(Avg('price'), Max('price'), Min('price'))
+
+# для вычисления значения для каждого объекта, annotate() не завершающая функция и её результатом будет QuerySet, который можно дальше менять
+q = Book.objects.annotate(Count('authors'))
+q[0].authors__count
+q[1].authors__count
+
+# Транзакции
+'''
+	Транзакция - это группа последовательных операций с базой данных.
+
+	Она может быть выполнена либо целиком и успешно, соблюдая целостность данных и независимо от параллельно идущих других транзакций, либо не выполнена вообще.
+
+	По умолчанию Django использует режим автоматической фиксации (autocommit). То есть аждый запрос сразу фиксируется в базе данных.
+
+	При включенном autocommit, если транзакция не активна, каждый SQL запрос обернут в транзакцию. То есть транзакция для каждого запроса не только создается, но и автоматически фиксируется, если запросы был успешно выполнен.
+
+	Если используется транзакция, то при получении запроса Django начинает транзакцию. Если ответ был создан без возникновения ошибок, Django фиксирует все ожидающие транзакции. Если функция представления вызывает исключение, Django откатывает все ожидающие транзакции.
+
+	Вы можете выполнять частичную фиксацию или откат изменений с помощью менеджера контекста atomic().
+
+	Создание транзакции для каждого запроса создает небольшую нагрузку на базу данных.
+
+	Только представления оборачиваются в транзакцию.
+
+	Декоратор @transaction.non_atomic_requests отключает транзакцию для указанного представления.
+
+	Иногда вам необходимо выполнить какие-либо действия связанные с текущей транзакцией в базе данных, но только при успешном коммите транзакции. Это может быть задача Celery, отправка электронного письма, или сброс кэша. Django предоставляет функцию on_commit(), которая позволяет добавить обработчик, вызываемый после успешного коммита транзакции.
+
+	Оказавшись в транзакции, можно зафиксировать выполненные изменения, используя функцию commit(), или отменить их через функцию rollback().
+'''
+@transaction.atomic # в этом представлении сработает транзакция для каждого действия
+def viewfunc(request):
+    do_stuff()
+
+def viewfunc(request):
+    do_stuff()
+
+    with transaction.atomic(): # использование менеджера контекста с транзакцией
+        do_more_stuff()
+
+def viewfunc(request):
+    create_parent()
+
+    try:
+        with transaction.atomic():
+            generate_relationships()
+    except IntegrityError:
+        handle_exception()
+
+    add_children()
+
+
+def do_something():
+    pass
+
+# использования функции если транзакция прошла успешно
+transaction.on_commit(do_something)
+transaction.on_commit(lambda: some_celery_task.delay('arg1'))
+
+# если будет откат первой транзакции, вторая не сработает
+with transaction.atomic():
+    transaction.on_commit(foo)
+
+    try:
+        with transaction.atomic():
+            transaction.on_commit(bar)
+            raise SomeError()
+    except SomeError:
+        pass
